dce3d88a38fb5db6bb4886363220bbab715ddfd4
Htetaungkyaw <htetaung200071@gmail.com>
2024-07-30 20:04:25 +0630

    fix production errors

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Table from "./pages/Table";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/table" element={<Table />} />
